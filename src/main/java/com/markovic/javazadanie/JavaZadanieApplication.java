5e77b8444cde888ad604eabe596698d5b122c7a1
Juraj Markovič <xmarkovicj@stuba.sk>
2025-11-10 10:43:21 +0100

    Initial, cisty zaklad bezi springboot

---
+++ src/main/java/com/markovic/javazadanie/JavaZadanieApplication.java
@@ -0,0 +1,13 @@
+package com.markovic.javazadanie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaZadanieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaZadanieApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/markovic/javazadanie/JavaZadanieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.markovic.javazadanie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaZadanieApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
